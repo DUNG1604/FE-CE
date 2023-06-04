bb2c17627514ec50533a05b09e055da0ec9faed9
DUNG1604 <dung0373691479@gmail.com>
2023-06-04 20:33:05 +0700

    update

--- src/Page/DinhHuong/DinhHuong.jsx
+++ src/Page/DinhHuong/DinhHuong.jsx
@@ -1,9 +1,9 @@
-import Header from "../../component/Header/Header"
+import Header1 from '../../component/Header/Header1'
 import './Dinhhuong.css'
 const DinhHuong = () => {
     return (
         <div>
-            <Header></Header>
+            <Header1></Header1>
             <div className="dinhhuong"></div>
         </div>
     )
