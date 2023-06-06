c02c0b77d42043c296e25614d4126265254d14c3
MaiNghiaa <Nghia.MT210179P@sis.hust.edu.vn>
2023-06-06 11:36:52 +0700

    update Commingsoon footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import VeChungToi from "./Page/VeChungToi/VeChungToi";
 import DinhHuong from "./Page/DinhHuong/DinhHuong";
-import { Routes, Route } from 'react-router-dom'
+import Commingsoon from "./Page/DinhHuong/Commingsoon";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
@@ -8,19 +9,13 @@ function App() {
       <Routes>
         <Route path="/" element={<VeChungToi />} />
         <Route path="/dinhhuong" element={<DinhHuong />} />
+        <Route path="/Commingsoon" element={<Commingsoon />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
 
 // import logo from './logo.svg';
 // import './App.css';
