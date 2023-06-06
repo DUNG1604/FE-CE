f34c0668e57126f6fd2964624ecc737e14951465
MaiNghiaa <Nghia.MT210179P@sis.hust.edu.vn>
2023-06-06 08:53:53 +0700

    edit footer content and supplement tam-nhin  & su-menh

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,73 +1,140 @@
-import './Footer.css'
+import "./Footer.css";
 const Footer = () => {
-    return (
-        <footer id="footer">
-            <div className="footer-top">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4 col-md-6 footer-contact">
-                            {/* <h3>CAREEREMPOWER</h3> */}
-                            <div className='footer_img'></div>
-                            <p>
-                                207 Giải Phóng, Đồng Tâm, Hai Bà Trưng, Hà Nội<br />
-                                {/* United States <br /><br /> */}
-                                <strong>Phone:</strong> 0888844426<br />
-                                <strong>Email:</strong> careerempower.neu@gmail.com<br />
-                            </p>
-                        </div>
-                        <div className="col-lg-2 col-md-6 footer-links">
-                            <h4>Về CareerEmpower</h4>
-                            <ul>
-                                <li><i className="bx bx-chevron-right" /> <a href="#about">Giới thiệu</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Liên hệ chi tiết</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Hỏi đáp</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Dịch vụ</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Điều khoản và dịch vụ</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Chính sách bảo mật</a></li>
-                            </ul>
-                            <h4></h4>
-
-                        </div>
-                        <div className="col-lg-2 col-md-6 footer-links">
-                            <h4>Dịch vụ</h4>
-                            <ul>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Đào tạo kĩ năng</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Khóa học tiếng Anh</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Hướng dẫn viết CV</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Thư viện CV</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Dịch vụ ...</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-lg-2 col-md-6 footer-links">
-                            <h4> Đối tác</h4>
-                            <ul>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Đối tác 1</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Đối tác 2</a></li>
-                                <li><i className="bx bx-chevron-right" /> <a href="#">Đối tác 3</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer id="footer">
+      <div className="footer-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 footer-contact">
+              {/* <h3>CAREEREMPOWER</h3> */}
+              <div className="footer_img"></div>
+              <p>
+                207 Giải Phóng, Đồng Tâm, Hai Bà Trưng, Hà Nội
+                <br />
+                {/* United States <br /><br /> */}
+                <strong>Phone:</strong> 0888844426
+                <br />
+                <strong>Email:</strong> careerempower.neu@gmail.com
+                <br />
+              </p>
             </div>
-            <div className="container d-md-flex py-4">
-                <div className="me-md-auto text-center text-md-start">
-                    <div className="copyright">
-                        © Copyright 2023 <strong><span>CareerEmpower</span></strong>. All Rights Reserved
-                    </div>
-                </div>
-                {/* <div className='links_social'> */}
-                <div className="social-links text-center text-md-right pt-3 pt-md-0">
-                    {/* <a target='blank' href="" className="twitter"><i className="bx bxl-twitter" /></a> */}
-                    <a target='blank' href="https://www.facebook.com/CareerEmpower/ " className="facebook"><i className="bx bxl-facebook" /></a>
-                    <a target='blank' href="https://www.instagram.com/careerempower.company/ " className="instagram"><i className="bx bxl-instagram" /></a>
-                    <a target='blank' href="https://www.linkedin.com/company/91164917/admin/ " className="linkedin"><i className="bx bxl-linkedin" /></a>
-                </div>
-                {/* </div> */}
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Về chúng tôi</h4>
+              <ul>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#about">Sứ mệnh</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Tầm nhìn</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Giá trị cốt lõi</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Triết lý thương hiệu</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">
+                    Con người
+                    <span class="sub"> (Tìm hiểu thêm - Page Life at CE)</span>
+                  </a>
+                </li>
+              </ul>
+              <h4></h4>
             </div>
-        </footer>
-
-
-
-    )
-}
-export default Footer
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Giải pháp</h4>
+              <ul>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Định hướng (Coming soon)</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Đào tạo (Coming soon)</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Kết nối việc làm (Coming soon)</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Tin tức</h4>
+              <ul>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Học bổng (Coming soon)</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Sự kiện (Coming soon)</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Cuộc thi (Coming soon)</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-6 footer-links">
+              <h4>Liên hệ</h4>
+              <ul>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Ứng tuyển (Coming soon)</a>
+                </li>
+                <li>
+                  <i className="bx bx-chevron-right" />
+                  <a href="#">Hợp tác (Coming soon)</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container d-md-flex py-4">
+        <div className="me-md-auto text-center text-md-start">
+          <div className="copyright">
+            © Copyright 2023
+            <strong>
+              <span>CareerEmpower</span>
+            </strong>
+            . All Rights Reserved
+          </div>
+        </div>
+        {/* <div className='links_social'> */}
+        <div className="social-links text-center text-md-right pt-3 pt-md-0">
+          {/* <a target='blank' href="" className="twitter"><i className="bx bxl-twitter" /></a> */}
+          <a
+            target="blank"
+            href="https://www.facebook.com/CareerEmpower/ "
+            className="facebook"
+          >
+            <i className="bx bxl-facebook" />
+          </a>
+          <a
+            target="blank"
+            href="https://www.instagram.com/careerempower.company/ "
+            className="instagram"
+          >
+            <i className="bx bxl-instagram" />
+          </a>
+          <a
+            target="blank"
+            href="https://www.linkedin.com/company/91164917/admin/ "
+            className="linkedin"
+          >
+            <i className="bx bxl-linkedin" />
+          </a>
+        </div>
+        {/* </div> */}
+      </div>
+    </footer>
+  );
+};
+export default Footer;
