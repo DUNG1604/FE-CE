c7976c01c891584d5123aca2bfcd95bbb5238fc4
nqthinh493 <nqthinh493@gmail.com>
2023-06-07 12:17:09 +0900

    add content

--- src/component/hero_section/hero.jsx
+++ src/component/hero_section/hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import './hero.css'
 const Hero = () => {
     return (
@@ -16,7 +18,11 @@ const Hero = () => {
                     <div className="carousel-container">
                         <h2 className="animate__animated animate__fadeInDown"><span>Dự án “Mùa hè không hối tiếc”</span></h2>
                         <p className="animate__animated animate__fadeInUp">Dự án dành riêng cho các bạn sinh viên trường Đại học Kinh tế Quốc dân, với nhiều vị trí hấp dẫn như: Content Marketing, Designer, Event, KOLs Nhân sự, Leader Truyền thông, Quay, dựng video Tiktok,... Bạn sẽ có cơ hội trải nghiệm làm việc trong môi trường chuyên nghiệp, sáng tạo và năng động, cùng với những người bạn đồng hành tài năng và nhiệt huyết. Liệu bạn có đủ can đảm để trở thành nhân tố khiến “Mùa hè không hối tiếc” trở thành ngọn lửa rực cháy trong mùa hè này?</p>
-                        <a href="/ungtuyen" className="btn-get-started animate__animated animate__fadeInUp scrollto">Tìm hiểu ngay!</a>
+                        <Link to="/ungtuyen">
+                            <a href="#" className="btn-get-started animate__animated animate__fadeInUp scrollto">Ứng tuyển</a>
+
+                        </Link>
+                        
                     </div>
                 </div>
                 {/* Slide 3 */}
@@ -24,7 +30,7 @@ const Hero = () => {
                     <div className="carousel-container">
                         <h2 className="animate__animated animate__fadeInDown"><span>NEU JOBS & INTERNSHIPS <br></br>Cơ hội việc làm, thực tập cho Gen Z tại NEU</span></h2>
                         <p className="animate__animated animate__fadeInUp">Group việc làm lớn nhất NEU với hơn 100k thành viên dành riêng cho sinh viên trường Đại học Kinh tế Quốc dân được thành lập bởi CareerEmpower với sứ mệnh kết nối việc làm đúng sở thích với mức lương xứng đáng. Đây là nơi chia sẻ kinh nghiệm, kiến thức và thông tin hữu ích về thị trường lao động, quy trình tuyển dụng và phát triển nghề nghiệp.</p>
-                        <a href=" https://www.facebook.com/groups/careerempower "target="_blank" className="btn-get-started animate__animated animate__fadeInUp scrollto">Tham gia ngay!</a>
+                        <a href=" https://www.facebook.com/groups/careerempower "target="_blank" className="btn-get-started animate__animated animate__fadeInUp scrollto">Tham gia ngay</a>
                     </div>
                 </div>
                 <a className="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
